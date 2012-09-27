9dc889ee32a6b9e3a8ad74ece62ca24f14fcca0e
Gautham Chandra <necromancer905@gmail.com>
2012-09-27 18:57:18 -0400

    added API_KEYS.java to source again

---
+++ src/org/hope/android/utils/API_KEYS.java
@@ -0,0 +1,12 @@
+package org.hope.android.utils;
+
+public class API_KEYS {
+
+	public static final String PARSE_APP_ID = "r3KiUBw36GDbazCpNmTWAQwbZDwxVmpa97oWRCib";
+	public static final String PARSE_CLIENT_KEY = "25FD0joFxtFRSqZhaXq3gm8yeibpThNzBSAnRKYJ";
+	
+	public static final String GENERIC_PWD = "org.hope";
+	
+	public static final String USER_PHONE = "User_Phone";
+	public static final String USER_LOC = "User_Location";
+}
