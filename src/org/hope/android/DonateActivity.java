b261ef53daf6a977b68708fd887dcf9f7d29dc81
raj <nycodes9@gmail.com>
2012-09-27 23:03:11 -0400

    changed date time format, still a little buggy on UI and a whole lot buggy on backend, logcat initialization to verify

--- src/org/hope/android/DonateActivity.java
+++ src/org/hope/android/DonateActivity.java
@@ -1,5 +1,7 @@
 package org.hope.android;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import com.parse.ParseException;
@@ -8,10 +10,17 @@ import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.app.TimePickerDialog.OnTimeSetListener;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -27,19 +36,22 @@ import android.widget.Toast;
 
 public class DonateActivity extends FragmentActivity {
 
+	public static final String TAG = "DonateActivity";
+
 	Context mContext;
-	
 	
 	Spinner donateFoodTypeSP;
 	String donateFoodTypeSPVal = "";
 	
-	DatePicker donateExpDateDP;
-	DatePicker donatePickUpDP;
-	TimePicker donatePickUpAfterDP;
-	TimePicker donatePickUpBeforeDP;
+	SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, MMM d, ''yy");
+	SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a");
+	
+	Button donateExpDateBtn;			Date expirationDate = new Date();
+	Button donatePickUpBtn;			Date pickupDate = new Date();
+	Button donatePickUpAfterBtn;		Date pickupAfterDate = new Date();
+	Button donatePickUpBeforeBtn;	Date pickupBeforeDate = new Date();
 	
 	EditText donateFoodWtET;
-	EditText donateAddInfoET;
 	EditText donateFoodNameET;
 	
 	Button donateBtn;
@@ -52,16 +64,97 @@ public class DonateActivity extends FragmentActivity {
         mContext = this;
         
         donateFoodNameET = (EditText) findViewById(R.id.donor_donate_foodNameET);
-        donateAddInfoET = (EditText) findViewById(R.id.donor_donate_pickupAddlnInfoET);
         donateFoodWtET = (EditText) findViewById(R.id.donor_donate_wtET);
-        donateExpDateDP = (DatePicker) findViewById(R.id.donor_donate_expDP);
-        donatePickUpDP = (DatePicker) findViewById(R.id.donor_donate_pickupDP);
-        donatePickUpBeforeDP = (TimePicker) findViewById(R.id.donor_donate_pickupBeforeTP);
-        donatePickUpAfterDP = (TimePicker) findViewById(R.id.donor_donate_pickupAfterTP);
+        donateExpDateBtn = (Button) findViewById(R.id.donor_donate_expDateBtn);
+        donatePickUpBtn = (Button) findViewById(R.id.donor_donate_pickupDateBtn);
+        donatePickUpBeforeBtn = (Button) findViewById(R.id.donor_donate_pickupBeforeTimeBtn);
+        donatePickUpAfterBtn = (Button) findViewById(R.id.donor_donate_pickupAfterTimeBtn);
         donateFoodTypeSP = (Spinner) findViewById(R.id.donor_donate_foodTypeSP);
         
+        donateExpDateBtn.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				final Calendar c = Calendar.getInstance();
+				int year = c.get(Calendar.YEAR);
+				int month = c.get(Calendar.MONTH);
+				int day = c.get(Calendar.DAY_OF_MONTH);
+				
+				new DatePickerDialog(mContext, new OnDateSetListener() {
+					
+					public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+						
+						Calendar cal = Calendar.getInstance();
+						cal.set(monthOfYear, monthOfYear, dayOfMonth, 24, 59);
+						
+						expirationDate = cal.getTime();
+						donateExpDateBtn.setText(dateFormat.format(expirationDate));
+					}
+				}, year, month, day).show();
+			}
+		});
         
+        donatePickUpBtn.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				final Calendar c = Calendar.getInstance();
+				int year = c.get(Calendar.YEAR);
+				int month = c.get(Calendar.MONTH);
+				int day = c.get(Calendar.DAY_OF_MONTH);
+				
+				new DatePickerDialog(mContext, new OnDateSetListener() {
+					
+					public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+						
+						Calendar cal = Calendar.getInstance();
+						cal.set(monthOfYear, monthOfYear, dayOfMonth, 24, 59);
+						
+						pickupDate = cal.getTime();
+						donatePickUpBtn.setText(dateFormat.format(pickupDate));
+					}
+				}, year, month, day).show();
+			}
+		});
         
+        donatePickUpBeforeBtn.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				final Calendar c = Calendar.getInstance();
+				c.setTime(pickupDate);
+				
+				new TimePickerDialog(mContext, new OnTimeSetListener() {
+					
+					public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+						c.set(Calendar.HOUR_OF_DAY, hourOfDay);
+						c.set(Calendar.MINUTE, minute);
+						
+						pickupBeforeDate = c.getTime();
+						
+						donatePickUpBeforeBtn.setText(timeFormat.format(pickupBeforeDate));
+					}
+				}, c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE), true).show();
+				
+			}
+		});
+        
+        donatePickUpAfterBtn.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				final Calendar c = Calendar.getInstance();
+				c.setTime(pickupDate);
+				
+				new TimePickerDialog(mContext, new OnTimeSetListener() {
+					
+					public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+						c.set(Calendar.HOUR_OF_DAY, hourOfDay);
+						c.set(Calendar.MINUTE, minute);
+						
+						pickupAfterDate = c.getTime();
+						donatePickUpAfterBtn.setText(timeFormat.format(pickupAfterDate));
+					}
+				}, c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE), true).show();
+				
+			}
+		});
         
 		donateFoodTypeSPVal = donateFoodTypeSP.getItemAtPosition(0).toString();
 		
@@ -83,38 +176,33 @@ public class DonateActivity extends FragmentActivity {
 			{
 				
 				String foodName = donateFoodNameET.getText().toString();
-				String additionalInfo = donateAddInfoET.getText().toString();
 				
 				int foodWeight = 0;
-				if (!donateFoodWtET.getText().toString().contains("-") && donateFoodWtET.getText().toString().trim().length() != 0) 
-				{
+				if (!donateFoodWtET.getText().toString().contains("-") && donateFoodWtET.getText().toString().trim().length() != 0) { 
 					foodWeight = Integer.parseInt(donateFoodWtET.getText().toString().trim());
 				}
 				
 				
 				//subtracting by 1900 as there is a weird calculation bug. Will fix later.
-				Date expireDate = new Date(donateExpDateDP.getYear() - 1900, 
-						donateExpDateDP.getMonth(), 
-						donateExpDateDP.getDayOfMonth());
-				Date pickupTimeBefore = new Date(donatePickUpDP.getYear() - 1900,
-						donatePickUpDP.getMonth(),
-						donatePickUpDP.getDayOfMonth(),
-						donatePickUpBeforeDP.getCurrentHour(),
-						donatePickUpBeforeDP.getCurrentMinute());
-				Date pickupTimeAfter = new Date(donatePickUpDP.getYear() - 1900,
-						donatePickUpDP.getMonth(),
-						donatePickUpDP.getDayOfMonth(),
-						donatePickUpAfterDP.getCurrentHour(),
-						donatePickUpAfterDP.getCurrentMinute());
-				
-				
+				/*Date expireDate = new Date(donateExpDateBtn.getYear() - 1900, 
+						donateExpDateBtn.getMonth(), 
+						donateExpDateBtn.getDayOfMonth());
+				Date pickupTimeBefore = new Date(donatePickUpBtn.getYear() - 1900,
+						donatePickUpBtn.getMonth(),
+						donatePickUpBtn.getDayOfMonth(),
+						donatePickUpBeforeBtn.getCurrentHour(),
+						donatePickUpBeforeBtn.getCurrentMinute());
+				Date pickupTimeAfter = new Date(donatePickUpBtn.getYear() - 1900,
+						donatePickUpBtn.getMonth(),
+						donatePickUpBtn.getDayOfMonth(),
+						donatePickUpAfterBtn.getCurrentHour(),
+						donatePickUpAfterBtn.getCurrentMinute());*/
 				
 				ParseUser currentUser = ParseUser.getCurrentUser();
 		        
 				//if we get null, then something has gone wrong because
 				//the user SHOULD be signed in.
-				if (currentUser == null) 
-		        {
+				if (currentUser == null) {
 		        	//print error message
 					return;
 		        }
@@ -133,7 +221,7 @@ public class DonateActivity extends FragmentActivity {
 				ParseObject donation = new ParseObject("contributions");
 				donation.put("food_name", foodName);
 				donation.put("food_type", donateFoodTypeSPVal);
-				donation.put("expire_date", expireDate);
+				donation.put("expire_date", expirationDate);
 				donation.put("food_weight", foodWeight);
 				
 				//donation.put("charity_name", "");
@@ -144,9 +232,8 @@ public class DonateActivity extends FragmentActivity {
 				donation.put("donor_address", currentUser.getString("address"));
 				donation.put("donor_phone", phoneNumber);
 				donation.put("donor_email", currentUser.getString("email"));
-				donation.put("available_start_time", pickupTimeBefore);
-				donation.put("available_end_time", pickupTimeAfter);
-				donation.put("additional_info", additionalInfo);
+				donation.put("available_start_time", pickupAfterDate);
+				donation.put("available_end_time", pickupBeforeDate);
 				
 				
 				//generate a quick toast message
@@ -217,4 +304,41 @@ public class DonateActivity extends FragmentActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+    
+	public static class TimePickerFragment extends DialogFragment implements OnTimeSetListener {
+
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			// Use the current time as the default values for the picker
+			final Calendar c = Calendar.getInstance();
+			int hour = c.get(Calendar.HOUR_OF_DAY);
+			int minute = c.get(Calendar.MINUTE);
+
+			// Create a new instance of TimePickerDialog and return it
+			return new TimePickerDialog(getActivity(), this, hour, minute, DateFormat.is24HourFormat(getActivity()));
+		}
+
+		public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+			Log.i(TAG, "Timepicker View : " + view);
+		}
+	}
+	
+	public static class DatePickerFragment extends DialogFragment implements OnDateSetListener {
+
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			// Use the current date as the default date in the picker
+			final Calendar c = Calendar.getInstance();
+			int year = c.get(Calendar.YEAR);
+			int month = c.get(Calendar.MONTH);
+			int day = c.get(Calendar.DAY_OF_MONTH);
+
+			// Create a new instance of DatePickerDialog and return it
+			return new DatePickerDialog(getActivity(), this, year, month, day);
+		}
+
+		public void onDateSet(DatePicker view, int year, int month, int day) {
+			Log.i(TAG, "DatePicker View : " + view);
+		}
+	}
 }
