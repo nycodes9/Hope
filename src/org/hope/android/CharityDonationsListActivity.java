3d391954959f58da3775e0332c7ba588e4ddd2ab
raj <nycodes9@gmail.com>
2012-09-27 23:00:03 -0400

    introduced checkbox for adding to list of donations

--- src/org/hope/android/CharityDonationsListActivity.java
+++ src/org/hope/android/CharityDonationsListActivity.java
@@ -2,14 +2,13 @@ package org.hope.android;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 
 import org.hope.android.utils.DonationsDS;
-import org.hope.android.utils.DonationsDS.FOOD_TYPES;
 
-import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
@@ -21,7 +20,6 @@ import android.content.Intent;
 import android.location.Address;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.ContactsContract.CommonDataKinds.Email;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -30,17 +28,27 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class CharityDonationsListActivity extends FragmentActivity {
 
 	private static final String TAG = "CharityDonationsListActivity";
 
+	private enum SORT_CATEGORY  {
+		EXP_DATE, QTY, PICKUP, TYPE, LOCATION;
+	}
+	
 	Context mContext;
+	
+	Spinner sortSP;
+	SORT_CATEGORY currentSortChoice = SORT_CATEGORY.EXP_DATE;
 	
 	ListView donationsListView;
 	View emptyView;
@@ -57,6 +65,41 @@ public class CharityDonationsListActivity extends FragmentActivity {
         setContentView(R.layout.charity_donations_list_layout);
         
         mContext = this;
+        
+        sortSP = (Spinner) findViewById(R.id.charity_donationsSortSP);
+        sortSP.setOnItemSelectedListener(new OnItemSelectedListener() {
+        	
+			public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+				switch (pos) {
+				case 0:
+					currentSortChoice = SORT_CATEGORY.EXP_DATE;
+					break;
+				case 1:
+					currentSortChoice = SORT_CATEGORY.QTY;
+					break;
+				case 2:
+					currentSortChoice = SORT_CATEGORY.PICKUP;
+					break;
+				case 3:
+					currentSortChoice = SORT_CATEGORY.TYPE;
+					break;
+				case 4:
+					currentSortChoice = SORT_CATEGORY.LOCATION;
+					break;
+
+				default:
+					currentSortChoice = SORT_CATEGORY.EXP_DATE;
+					break;
+				}
+				
+				sortDonation(currentSortChoice);
+				
+			}
+
+			public void onNothingSelected(AdapterView<?> arg0) {
+				
+			}
+		});
         
         donationsAdapter = new DonationsDataAdapter();
         
@@ -184,6 +227,47 @@ public class CharityDonationsListActivity extends FragmentActivity {
 		}.execute();
     }
     
+    public void sortDonation(final SORT_CATEGORY by) {
+
+		Collections.sort(donationList, new Comparator<DonationsDS>() {
+
+			public int compare(DonationsDS lhs, DonationsDS rhs) {
+
+				int returnDummy = 0;
+
+				switch (by) {
+				case EXP_DATE:
+					returnDummy = lhs.expDate.compareTo(rhs.expDate);
+					break;
+
+				case QTY:
+					returnDummy = lhs.wtQty > rhs.wtQty ? 0 : 1;
+					break;
+					
+				case PICKUP:
+					returnDummy = lhs.pickupDate.compareTo(rhs.expDate);
+					break;
+					
+				case TYPE:
+					returnDummy = lhs.type.compareTo(rhs.type);
+					break;
+					
+				case LOCATION:
+					returnDummy = lhs.pickupLocAndroidAddress.toString().compareTo(lhs.pickupLocAndroidAddress.toString()); 
+					break;
+				}
+
+				return returnDummy;
+
+			}
+			
+		});
+		
+		donationsAdapter.notifyDataSetChanged();
+		donationsListView.invalidateViews();
+
+	}
+    
     class DonationsDataAdapter extends BaseAdapter {
 
 		public int getCount() {
@@ -205,21 +289,17 @@ public class CharityDonationsListActivity extends FragmentActivity {
 			if (convertView == null) {
 				convertView = LayoutInflater.from(mContext).inflate(R.layout.donation_item_layout, parent, false);
 				holder = new ViewHolder();
-				holder.donorEmailTV = (TextView)convertView.findViewById(R.id.donation_donorEmailTV);
+				holder.foodNameTV = (TextView)convertView.findViewById(R.id.donation_pickupInfoTV);
 				holder.foodTypeTV = (TextView)convertView.findViewById(R.id.donation_foodTypeTV);
 				holder.expDateTV = (TextView)convertView.findViewById(R.id.donation_expTV);
 				holder.wtQtyTV = (TextView)convertView.findViewById(R.id.donation_wtTV);
 				holder.pickupDateTV = (TextView)convertView.findViewById(R.id.donation_pickupDateTV);
 				holder.pickupBtwnTV = (TextView)convertView.findViewById(R.id.donation_pickupTimeTV);
-				holder.extraInfoTV = (TextView)convertView.findViewById(R.id.donation_pickupInfoTV);
-				holder.pickupBtnTV = (Button)convertView.findViewById(R.id.donation_pickupBtn);
+				holder.reviewBtn = (Button)convertView.findViewById(R.id.donation_reviewBtn);
 				convertView.setTag(holder);
 			} else {
 				holder = (ViewHolder)convertView.getTag();
 			}
-			
-			holder.donorEmailTV.setText(donationList.get(position).donorEmail);
-			
 			
 			holder.foodTypeTV.setText(donationList.get(position).type.toLowerCase());
 			holder.expDateTV.setText(sdatef.format(donationList.get(position).expDate));
@@ -230,9 +310,9 @@ public class CharityDonationsListActivity extends FragmentActivity {
 					+ stimef.format(donationList.get(position).pickupBeforeTime) ;
 			
 			holder.pickupBtwnTV.setText(timeBtwn);
-			holder.extraInfoTV.setText(donationList.get(position).pickupInfoExtraStr);
+			holder.foodNameTV.setText(donationList.get(position).pickupInfoExtraStr);
 			
-			holder.pickupBtnTV.setOnClickListener(new OnClickListener() {
+			holder.reviewBtn.setOnClickListener(new OnClickListener() {
 				
 				public void onClick(View v) {
 					/**
@@ -250,14 +330,14 @@ public class CharityDonationsListActivity extends FragmentActivity {
     
     static class ViewHolder {
     	
-    	TextView donorEmailTV;
+    	TextView foodNameTV;
     	TextView foodTypeTV;
     	TextView expDateTV;
     	TextView wtQtyTV;
     	TextView pickupDateTV;
     	TextView pickupBtwnTV;
-    	TextView extraInfoTV;
-    	Button pickupBtnTV;
+    	CheckBox addToPickupListCB; 
+    	Button reviewBtn;
     	
     }
     
